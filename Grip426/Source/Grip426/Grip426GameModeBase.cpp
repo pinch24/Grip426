d78daf7be55ccb8135c8ebce0ad2069aab965821
pinch24 <pinch24@gmail.com>
2023-06-01 11:01:40 +0900

    Update .gitignore

---
+++ Grip426/Source/Grip426/Grip426GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Grip426GameModeBase.h"
+
---
+++ Grip426/Source/Grip426/Grip426GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Grip426GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRIP426_API AGrip426GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
